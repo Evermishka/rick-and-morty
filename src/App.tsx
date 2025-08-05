6321817a8ac18078570e7d1783c605c972fbaa85
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-05 17:30:59 +0300

    Добавляет NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "./layouts";
 import { ROUTES } from "./constants";
-import { CategoryPage, CharacterPage, EpisodePage, LocationPage, MainPage } from "./pages";
+import { CategoryPage, CharacterPage, EpisodePage, LocationPage, MainPage, NotFoundPage } from "./pages";
 import { data } from "./data";
 
 function App() { 
@@ -16,6 +16,7 @@ function App() {
           <Route path={ `${ROUTES.EPISODES}/:id` } element={<EpisodePage episodes={data.episodes} />} />
           <Route path={ ROUTES.LOCATIONS } element={<CategoryPage categoryData={data.locations} />} />
           <Route path={ `${ROUTES.LOCATIONS}/:id` } element={<LocationPage locations={data.locations} />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>        
       </Routes>     
   )
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export * from './character-page/character-page';
 export * from './episode-page/episode-page';
 export * from './location-page/location-page';
 export * from './main-page/main-page';
+export * from './not-found-page/not-found-page';
