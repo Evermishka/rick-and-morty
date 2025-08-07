7d41ff27112c7ec38ffb503625ebc2a1db1ae73c
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-07 17:29:05 +0300

    Добавляет на странице по категориям сортировку по createdASC и DESC

--- src/pages/category-page/category-page.tsx
+++ src/pages/category-page/category-page.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useMemo, useCallback } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import type { Categories } from '../../types';
 import styles from './category-page.module.css';
 
@@ -6,14 +7,43 @@ type CategoryPageProps = {
     categoryData: Categories;
 }
 
+type Sorting = 'asc' | 'desc' | 'none';
+
 export const CategoryPage = ({ categoryData }: CategoryPageProps) => {
+    const [searchParams, setSearchParams] = useSearchParams({ sorting: 'none' });
+    const sorting = searchParams.get('sorting') as Sorting || 'none';
+
+    const handleSorting = useCallback((newSorting: Sorting) => {
+        setSearchParams({ sorting: newSorting });
+    }, [setSearchParams]);
+
+    const sortedData = useMemo(() => {
+        if (sorting === 'none') return [...categoryData];
+
+        return [...categoryData].sort((a, b) => {
+            if (sorting === 'asc') {
+                return new Date(a.created).valueOf() - new Date(b.created).valueOf();
+            } else if (sorting === 'desc') {
+                return new Date(b.created).valueOf() - new Date(a.created).valueOf();
+            }
+            return 0;
+        });
+    }, [categoryData, sorting]);
+
     return (
-        <ul>
-            {categoryData.map(item => (
-                <li key={item.id} className={styles.item}>
-                    <Link to={`${item.id}`} className={styles.link}>{item.name}</Link>
-                </li>
-            ))}
-        </ul>
-    )
-}
+        <>
+            <div className={styles.sorting}>
+                <p>Sort by:</p>
+                <button className={styles.sortingButton} onClick={() => handleSorting('asc')}>Ascending</button>
+                <button className={styles.sortingButton} onClick={() => handleSorting('desc')}>Descending</button>
+            </div>
+            <ul>
+                {sortedData.map(item => (
+                    <li key={item.id} className={styles.item}>
+                        <Link to={`${item.id}`} className={styles.link}>{item.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
