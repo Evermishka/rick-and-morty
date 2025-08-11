4f5a3d17d935bac0f8bc09081d33cc6d663c495b
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-11 12:04:53 +0300

    Добавляет AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/auth-provider';
 import { MainLayout } from './layouts';
 import { ROUTES } from './constants';
 import {
@@ -14,37 +15,39 @@ import { data } from './data';
 
 function App() {
     return (
-        <Routes>
-            <Route element={<MainLayout />}>
-                <Route path={ROUTES.MAIN} element={<MainPage />} />
-                <Route
-                    path={ROUTES.CHARACTERS}
-                    element={<CategoryPage categoryData={data.characters} />}
-                />
-                <Route
-                    path={`${ROUTES.CHARACTERS}/:id`}
-                    element={<CharacterPage characters={data.characters} />}
-                />
-                <Route
-                    path={ROUTES.EPISODES}
-                    element={<CategoryPage categoryData={data.episodes} />}
-                />
-                <Route
-                    path={`${ROUTES.EPISODES}/:id`}
-                    element={<EpisodePage episodes={data.episodes} />}
-                />
-                <Route
-                    path={ROUTES.LOCATIONS}
-                    element={<CategoryPage categoryData={data.locations} />}
-                />
-                <Route
-                    path={`${ROUTES.LOCATIONS}/:id`}
-                    element={<LocationPage locations={data.locations} />}
-                />
-                <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Route>
-        </Routes>
+        <AuthProvider>
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path={ROUTES.MAIN} element={<MainPage />} />
+                    <Route
+                        path={ROUTES.CHARACTERS}
+                        element={<CategoryPage categoryData={data.characters} />}
+                    />
+                    <Route
+                        path={`${ROUTES.CHARACTERS}/:id`}
+                        element={<CharacterPage characters={data.characters} />}
+                    />
+                    <Route
+                        path={ROUTES.EPISODES}
+                        element={<CategoryPage categoryData={data.episodes} />}
+                    />
+                    <Route
+                        path={`${ROUTES.EPISODES}/:id`}
+                        element={<EpisodePage episodes={data.episodes} />}
+                    />
+                    <Route
+                        path={ROUTES.LOCATIONS}
+                        element={<CategoryPage categoryData={data.locations} />}
+                    />
+                    <Route
+                        path={`${ROUTES.LOCATIONS}/:id`}
+                        element={<LocationPage locations={data.locations} />}
+                    />
+                    <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Route>
+            </Routes>
+        </AuthProvider>
     );
 }
 
---
+++ src/context/auth-provider.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+type LoginFnType = (newUser: string, Cb: Function) => void;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+type LogoutFnType = (Cb: Function) => void;
+
+type AuthContextType = {
+    user: string | null;
+    login: LoginFnType;
+    logout: LogoutFnType;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+type AuthProviderProps = {
+    children: React.ReactNode;
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+    const [user, setUser] = useState<string | null>(null);
+
+    const login: LoginFnType = (newUser, Cb) => {
+        setUser(newUser);
+        Cb();
+    };
+
+    const logout: LogoutFnType = (Cb) => {
+        setUser(null);
+        Cb();
+    };
+
+    const value = {
+        user,
+        login,
+        logout,
+    };
+
+    return <AuthContext value={value}>{children}</AuthContext>;
+};
